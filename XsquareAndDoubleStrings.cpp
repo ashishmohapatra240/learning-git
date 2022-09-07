d9bf9361ffc0ca4684ff231e1c30234068b2f758
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2022-09-07 16:17:37 +0530

    DSA Karo guys!

---
+++ XsquareAndDoubleStrings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string P, A, B;
+	cin>>P;
+	for(int i=0; i<2;i++){
+		A[i]=P[i];
+	}
+	for(int i=2; i<5;i++){
+		B[i]=P[i];
+	}
+
+}
